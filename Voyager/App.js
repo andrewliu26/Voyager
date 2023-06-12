f6923882ebb60acad0aca7d056eea327777aad9d
andrewliu26 <aliu26@bu.edu>
2023-06-12 00:54:52 -0400

    configured launch.json file for debugging
    Next step is adding core components.

--- Voyager/App.js
+++ Voyager/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  //let x = 1;
+  console.log("App executed");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to Voyager</Text>
       <StatusBar style="auto" />
     </View>
   );
