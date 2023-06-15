abfc9f5b39e2168cb8d137b0e3908e2e4eea9d5c
laimao19 <laimao19@bu.edu>
2023-06-15 11:24:11 -0400

    readjusted the code again; still not working with calling the specific code

--- prototype/api/routes/weatherAPI.js
+++ prototype/api/routes/weatherAPI.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const weatherConfig = require('../config/weatherConfig');
+var weather = "";
 
 //weather details and city details
 router.get('/', async(req, res, next) => {
@@ -12,22 +13,22 @@ router.get('/', async(req, res, next) => {
     let rawData = await fetch(weatherConfig.weatherCityURL + weatherConfig.weatherConfigkeyCity + "q=london", requestOptions)
         .then(response => response.json())
         .then(result => {
-            console.log(JSON.parse(JSON.stringify(result[0])));
-            let weather = JSON.parse(JSON.stringify(result[0]));
-            return JSON.parse(JSON.stringify(result[0]));
+            weather = result[0];
+            let cityData = fetch(weatherConfig.weatherURL + weather + weatherConfig.weatherConfigkey, requestOptions)
+                .then(response => response.json())
+                .then(result => {
+                    //console.log(result);
+                    return result;
+                })
+                .then(weatherData => res.json(weatherData))
+                .catch(error => console.log('error', error));
+            return result[0];
+
         })
         .then(locationData => res.json(locationData))
         .catch(error => console.log('error', error));
 
 
-    /*let cityData = await fetch(weatherConfig.weatherURL + req.query.key + weatherConfig.weatherConfigkeyDetails, requestOptions)
-        .then(response => response.json())
-        .then(result => {
-            console.log(result);
-            return result;
-        })
-        .then(weatherData => res.json(weatherData))
-        .catch(error => console.log('error', error));*/
 })
 
 module.exports = router;
