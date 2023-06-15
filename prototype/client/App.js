ce9c803f9774147576e0c85ffd2618d7da37ecbe
andrewliu26 <aliu26@bu.edu>
2023-06-15 09:55:44 -0400

    updated front end with search bar for location

--- prototype/client/App.js
+++ prototype/client/App.js
@@ -1,38 +1,84 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Pressable } from 'react-native';
 
 export default function App() {
   const [number, onChangeNumber] = React.useState('');
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Check the weather in:</Text>
-      <TextInput
-        style={styles.input}
-        textAlign={'center'}
-        onChangeText={onChangeNumber}
-        value={number}
-        placeholder="Enter a location"
-        keyboardType="numeric"
-      />
-      
-      <StatusBar style="auto" />
+      <View style={styles.searchBar}>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="Enter a location"
+          keyboardType="numeric"
+        />
+      </View>
+      <View style={styles.searchButton}>
+        <Pressable
+          onPress={() => {console.log('Search pressed')}}
+          style={({pressed}) => [
+            {
+              backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white',
+            },
+            styles.wrapperCustom,
+          ]}>
+          {({pressed}) => (
+            <Text style={styles.text}>{pressed ? 'Search' : 'Search'}</Text>
+          )}
+        </Pressable>
+      </View>
+        <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  // View styles
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  searchBar: {
+    backgroundcolor: '#A4EE82',
+    width: 200,
+    height: 100,
+    margin: 10,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  input: {
-    flex: 1,
-    padding: 10,
-    backgroundColor: '#e8e8e8',
-    
+  searchButton: {
+    backgroundcolor: '#A4EE82',
+    width: 60,
+    height: 100,
+    margin: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+
+  // Component styles
+  input: {
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: '#bfbfbf',
+    margin: 10,
+    height: 40,
+    width: 200,
+  },
+  wrapperCustom: {
+    borderRadius: 10,
+    padding: 10,
+    backgroundColor: '#299BF7',
+    height: 40,
+    width: 70,
+  },
+  text: {
+    color: '#fff'
+  }
 });
