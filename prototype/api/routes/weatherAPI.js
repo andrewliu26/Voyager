cd166bd12516ccec99f362104bbd8982c53c429d
laimao19 <laimao19@bu.edu>
2023-06-15 14:52:31 -0400

    trying a new method of calling the api since the previous one wasn't working, haven't implemented a call into the app.js file yet and will be testing for correctness.

--- prototype/api/routes/weatherAPI.js
+++ prototype/api/routes/weatherAPI.js
@@ -1,11 +1,41 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const router = express.Router();
 const weatherConfig = require('../config/weatherConfig');
-var weather = "";
+const apiKey = weatherConfig.weatherConfigkey;
 
 //weather details and city details
-router.get('/', async(req, res, next) => {
-    const requestOptions = {
+router.get('/:location', async(req, res, next) => {
+    try {
+        const locationURL = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${req.params.location}`;
+        const locationResponse = await fetch(locationURL);
+        const locationData = await locationResponse.json();
+
+        if (locationData.length == 0) {
+            throw new Error('Location not found');
+        }
+
+        const locationKey = locationData[0].Key;
+
+        const conditionsURL = `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`;
+        const conditionsResponse = await fetch(conditionsURL);
+        const conditionsData = await conditionsResponse.json();
+
+        const temperature = conditions[0].Temperature.Metric.Value;
+        const weatherText = conditions[0].WeatherText;
+
+        const response = {
+            location: req.params.location,
+            temperature,
+            weatherText
+        };
+
+        res.json(response);
+    } catch(error) {
+        res.status(404).json({error: error.message});
+    }
+
+    /*const requestOptions = {
         method: 'GET',
         redirect: 'follow'
     };
@@ -26,7 +56,7 @@ router.get('/', async(req, res, next) => {
 
         })
         .then(locationData => res.json(locationData))
-        .catch(error => console.log('error', error));
+        .catch(error => console.log('error', error));*/
 
 
 })
