47e15cfd97a587dc717c8dd73e418376935108dd
laimao19 <laimao19@bu.edu>
2023-06-21 10:27:51 -0400

    deleted unecessary files, modified chatgptapi file to acount for more characters in the messsage that is input; also modified the app.js file for the '/' for the chatGPTrouter to be /api instead

--- Voyager/voyager-be/routes/chatGPTAPI.js
+++ Voyager/voyager-be/routes/chatGPTAPI.js
@@ -9,13 +9,13 @@ const axios = require('axios');
 
 const apiKey = chatGPTConfig.chatGPTkey;
 
-router.post('/chat', async (req, res) => {
+router.post('/create-itinerary', async (req, res) => {
    try {
-       const userInput = req.body.message;
+       const { userMessage } = req.body;
 
        const response = await axios.post(chatGPTConfig.chatGPTAPIEndpoint, {
-           prompt: userInput,
-           max_tokens: 50,
+           prompt: userMessage,
+           max_tokens: 100,
            temperature: 0.7,
            n: 1
        }, {
@@ -25,28 +25,13 @@ router.post('/chat', async (req, res) => {
            },
        });
 
-       const chatResponse = response.data.choices[0].text.trim();
+       const itinerary = response.data.choices[0].text.trim();
 
-       res.json({ message: chatResponse });
+       res.json({ itinerary });
    } catch(error) {
        console.error('Error:', error.message);
        res.status(500).json({error: 'Something went wrong. '});
    }
-
-    /*const { message } = req.body;
-    console.log(message);
-    userInterface.prompt();
-    userInterface.on("line", async input => {
-        const completion = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [
-                {role: "user", content: input}
-            ]
-        })
-        res.send(completion.data.choices[0].message);
-        userInterface.prompt();
-    })*/
-
 
 });
 
