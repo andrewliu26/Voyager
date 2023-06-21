7faa70e78094ce98fcc98ca980f4dbe25865abcb
andrewliu26 <aliu26@bu.edu>
2023-06-21 10:47:45 -0400

    created base itinerary generation screen
    Currently lacks proper functionality

--- Voyager/front-end/App.js
+++ Voyager/front-end/App.js
@@ -1,6 +1,10 @@
+/*
+import react from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, SafeAreaView } from 'react-native';
-
+*/
+import * as axios from "react-native/Libraries/TurboModule/TurboModuleRegistry";
+/*
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
@@ -18,3 +22,54 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+*/
+
+import React, { useState } from 'react';
+import { View, SafeAreaView, StyleSheet, Text, TextInput, Button } from 'react-native';
+
+const App = () => {
+  const [userMessage, setUserMessage] = useState('');
+  const [itinerary, setItinerary] = useState('');
+
+  const handleCreateItinerary = async () => {
+    try {
+      const response = await axios.post('http://localhost:3000/api/create-itinerary', { userMessage });
+      setItinerary(response.data.itinerary);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // styles for components
+  const styles = StyleSheet.create({
+      container: {
+          flex: 1,
+          backgroundColor: '#fff',
+          alignItems: 'center',
+          justifyContent: 'center',
+      },
+  });
+
+  return (
+      <SafeAreaView style={styles.container}>
+        <TextInput
+            style={{ width: 200,
+                    height: 40,
+                    padding: 10,
+                    margin: 10,
+                    borderRadius: 10,
+                    borderColor: 'gray',
+                    borderWidth: 1
+            }}
+            onChangeText={text => setUserMessage(text)}
+            value={userMessage}
+            placeholder="Enter a message"
+        />
+        <Button title="Create Itinerary" onPress={handleCreateItinerary} />
+        <Text>Generated Itinerary:</Text>
+        <Text>{itinerary}</Text>
+      </SafeAreaView>
+  );
+};
+
+export default App;
