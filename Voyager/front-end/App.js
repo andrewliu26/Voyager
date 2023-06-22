09cbcf07530a80e576c2f6589fb7df4044d822fd
andrewliu26 <aliu26@bu.edu>
2023-06-22 16:20:36 -0400

    updated front end App.js, added axios import statements

--- Voyager/front-end/App.js
+++ Voyager/front-end/App.js
@@ -9,7 +9,7 @@ const App = () => {
 
   const handleCreateItinerary = async () => {
     try {
-      const response = await axios.post('http://localhost:3000/api/create-itinerary', { userMessage });
+      const response = await axios.post('http://localhost:3000/create-itinerary', { userMessage });
       setItinerary(response.data.itinerary);
     } catch (error) {
       console.error(error);
@@ -28,22 +28,29 @@ const App = () => {
 
   return (
       <SafeAreaView style={styles.container}>
-        <TextInput
-            style={{ width: 200,
-                    height: 40,
-                    padding: 10,
-                    margin: 10,
-                    borderRadius: 10,
-                    borderColor: 'gray',
-                    borderWidth: 1
-            }}
-            onChangeText={text => setUserMessage(text)}
-            value={userMessage}
-            placeholder="Enter a message"
-        />
-        <Button title="Create Itinerary" onPress={handleCreateItinerary} />
-        <Text>Generated Itinerary:</Text>
-        <Text>{itinerary}</Text>
+          <View style={{ width: '100%', flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center' }}>
+
+          </View>
+          <View style={{ width: '100%', flex: 5, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center' }}>
+              <TextInput
+                  style={{ width: 200,
+                      height: 40,
+                      padding: 10,
+                      margin: 10,
+                      borderRadius: 10,
+                      borderColor: 'gray',
+                      borderWidth: 1
+                  }}
+                  onChangeText={text => setUserMessage(text)}
+                  value={userMessage}
+                  placeholder="Enter a message"
+              />
+              <Button title="Create Itinerary" onPress={handleCreateItinerary} />
+          </View>
+          <View style={{ width: '100%', flex: 3, backgroundColor: '#fff', alignItems: 'center' }}>
+              <Text>Generated Itinerary:</Text>
+              <Text>{itinerary}</Text>
+          </View>
       </SafeAreaView>
   );
 };
