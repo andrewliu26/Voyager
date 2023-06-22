10ccb863e59246a873060a986346194338038f7b
laimao19 <laimao19@bu.edu>
2023-06-22 17:18:48 -0400

    added in a user schema so that we can check if a user exists and so that we can check for functionality of login for fb

--- Voyager/voyager-be/routes/facebookOauth.js
+++ Voyager/voyager-be/routes/facebookOauth.js
@@ -2,15 +2,43 @@ const express = require('express');
 const passport = require('passport');
 const FaceBookStrategy = require('passport-facebook').Strategy;
 const facebookConfig = require('../config/facebookConfig');
+const { User, findOne, save } = require('../files/fbUser');
+
 
 const router = express.Router();
 
 passport.use(new FaceBookStrategy({
-    clientID: facebookConfig.facebookclientID,
+    clientID: facebookConfig.facebookClientID,
     clientSecret: facebookConfig.facebookclientSecret,
-    callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    profileFields: ['id', 'displayName', 'email']
 }, (accessToken, refreshToken, profile, done) => {
-    //Handle user authentication or registration logic here
+    //Handle user authentication or registration logic
+    //check if user is already registered
+    User.findOne({facebookId: profile.id}, (err, user) => {
+        if(err) {
+            return done(err);
+        }
+
+        if(user) {
+            //user is registered
+            return done(null, user);
+        }
+
+        //user is not registered, create new user
+        const newUser = new User({
+            facebookId: profile.id,
+            name: profile.displayName,
+            email: profile.emails[0].value
+        });
+
+        newUser.save((err) => {
+            if(err) {
+                return done(err);
+            }
+            return done(null, newUser);
+        });
+    });
 }));
 
 router.use(passport.initialize());
